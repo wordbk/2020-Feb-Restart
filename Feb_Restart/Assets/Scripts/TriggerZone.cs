41c13af8ae8b0cf7e2e5e5e5e6b50cb9dc1d0a96
BK <bk@wordbk.com>
2020-02-19 22:25:44 -0500

    Added unity project

---
+++ Feb_Restart/Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public Animator bridgeAnim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            bridgeAnim.SetBool("OpenStairs", true);
+        }
+    }
+}
